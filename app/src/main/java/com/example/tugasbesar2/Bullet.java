69ab2f2d50d440a8d8610fed4e747c84e74cc0da
Harry Senjaya <harxsenjaya56@gmail.com>
2019-10-29 22:06:39 +0700

    bullet

---
+++ app/src/main/java/com/example/tugasbesar2/Bullet.java
@@ -0,0 +1,29 @@
+package com.example.tugasbesar2;
+
+public class Bullet {
+    private int x;
+    private int y;
+    private int width;
+
+
+    public Bullet(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return this.x;
+    }
+
+    public int getY(){
+        return this.y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
